43b4c838a023e024685f73d62eb99030dad00722
Tecco <glaypppgod56d@gmail.com>
2016-03-02 19:39:13 +0900

    create listview & add string-array

--- app/src/main/java/com/tecc0/libraryplay/fragment/HomeFragment.java
+++ app/src/main/java/com/tecc0/libraryplay/fragment/HomeFragment.java
@@ -7,8 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.tecc0.libraryplay.R;
+
+import static butterknife.ButterKnife.findById;
 
 public class HomeFragment extends Fragment {
 
@@ -22,14 +26,15 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ListView lv = findById(view, R.id.listView);
+        lv.setAdapter(new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_checked, getResources().getStringArray(R.array.study_libraries_name)));
+        return view;
     }
 
 }
