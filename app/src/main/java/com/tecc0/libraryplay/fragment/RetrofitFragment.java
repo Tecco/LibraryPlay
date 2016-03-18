1bbee81240e042542373066faa141b6c95f24cd6
Tecco <glaypppgod56d@gmail.com>
2016-03-18 15:45:40 +0900

    use Rx (not using Integer

--- app/src/main/java/com/tecc0/libraryplay/fragment/RetrofitFragment.java
+++ app/src/main/java/com/tecc0/libraryplay/fragment/RetrofitFragment.java
@@ -122,11 +122,15 @@ public class RetrofitFragment extends Fragment {
 //                                    })
 //                                    .toList().toBlocking().single();
 //
-                            List<WeatherData> weatherList = Observable.range(0, WEATHER_API_GET_COUNT)
-                                    .map(i -> {
-                                        DayWeather w = weather.dayWeatherList.get(i);
-                                        return new WeatherData(w.weather.get(0).icon, w.temp.max, w.temp.min, w.weather.get(0).main);
-                                    })
+//                            List<WeatherData> weatherList3 = Observable.range(0, WEATHER_API_GET_COUNT)
+//                                    .map(i -> {
+//                                        DayWeather w = weather.dayWeatherList.get(i);
+//                                        return new WeatherData(w.weather.get(0).icon, w.temp.max, w.temp.min, w.weather.get(0).main);
+//                                    })
+//                                    .toList().toBlocking().single();
+//
+                            List<WeatherData> weatherList = Observable.from(weather.dayWeatherList)
+                                    .map(w -> new WeatherData(w.weather.get(0).icon, w.temp.max, w.temp.min, w.weather.get(0).main))
                                     .toList().toBlocking().single();
 
                             // Observable使わないやつ
