cbbecb4b7a0ac70b313db16b428e4b9f7c73fb66
Tecco <glaypppgod56d@gmail.com>
2016-03-23 11:24:31 +0900

    add Espresso test

--- app/src/androidTest/java/com/tecc0/libraryplay/ApplicationTest.java
+++ app/src/androidTest/java/com/tecc0/libraryplay/ApplicationTest.java
@@ -1,13 +1,43 @@
 package com.tecc0.libraryplay;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private MainActivity activity;
+
+    public ApplicationTest(){
+        super(MainActivity.class);
+    }
+
+    @Before
+    public void setUp() throws Exception{
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        this.activity = getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        super.tearDown();
+    }
+
+    @Test
+    public void checkPrecondition() {
+        Espresso.onView(ViewMatchers.withId(R.id.home_title)).check(ViewAssertions.matches(ViewMatchers.withText("Study Libraries")));
     }
 }
