eb51535b65b02757b5c6450ce49137d465948e58
Tecco <glaypppgod56d@gmail.com>
2016-03-25 16:30:11 +0900

    add coodinator tests & static impot

--- app/src/androidTest/java/com/tecc0/libraryplay/ApplicationTest.java
+++ app/src/androidTest/java/com/tecc0/libraryplay/ApplicationTest.java
@@ -2,15 +2,12 @@ package com.tecc0.libraryplay;
 
 import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.contrib.NavigationViewActions;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.widget.DrawerLayout;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.Gravity;
 
-import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,8 +17,12 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -50,9 +51,9 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
     @Test
     public void testFirstDisplay() {
         // HomeのTextViewのタイトルがあっているか
-        onView(withId(R.id.home_title)).check(ViewAssertions.matches(withText("Study Libraries")));
+        onView(withId(R.id.home_title)).check(matches(withText("Study Libraries")));
         // FABが表示されているか
-        onView(withId(R.id.fab)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.fab)).check(matches(isDisplayed()));
         // FABをクリック
         onView(withId(R.id.fab)).perform(click());
     }
@@ -67,7 +68,7 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
     public void testShowGallery() {
         openDrawer();
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_gallery));
-        onView(withId(R.id.gallery_listview)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.gallery_listview)).check(matches(isDisplayed()));
         closeDrawer();
     }
 
@@ -75,7 +76,7 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
     public void testShowRetrofit() {
         openDrawer();
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_retrofit));
-        onView(withId(R.id.retrofit_listview)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.retrofit_listview)).check(matches(isDisplayed()));
         closeDrawer();
     }
 
@@ -83,9 +84,9 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
     public void testShowTour() {
         openDrawer();
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_tour));
-        onView(withId(R.id.introViewPager)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.introViewPager)).check(matches(isDisplayed()));
         // skipボタンが表示
-        onView(withId(R.id.skipIntroButton)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.skipIntroButton)).check(matches(isDisplayed()));
         onView(withId(R.id.introViewPager)).perform(swipeLeft());
         onView(withId(R.id.introViewPager)).perform(swipeRight());
 
@@ -93,21 +94,24 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
         onView(withId(R.id.introViewPager)).perform(swipeLeft());
         onView(withId(R.id.introViewPager)).perform(swipeLeft());
         // skipボタンが非表示
-        onView(withId(R.id.skipIntroButton)).check(ViewAssertions.matches(Matchers.not(ViewMatchers.isDisplayed())));
+        onView(withId(R.id.skipIntroButton)).check(matches(not(isDisplayed())));
 
         // doneボタンが表示・クリック
-        onView(withId(R.id.doneSlideButton)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.doneSlideButton)).check(matches(isDisplayed()));
         onView(withId(R.id.doneSlideButton)).perform(click());
 
         // TourActivityがfinish()してMainActivityに遷移
-        onView(withId(R.id.fab)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.fab)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testShowCoodinator() {
         openDrawer();
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_coordinator));
-        onView(withId(R.id.coordinator_fab)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.coordinator_fab)).check(matches(isDisplayed()));
+        // swipeするとfabが消える
+        onView(withId(R.id.app_bar)).perform(swipeUp());
+        onView(withId(R.id.coordinator_fab)).check(matches(not(isDisplayed())));
         closeDrawer();
     }
 
@@ -115,7 +119,7 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
     public void testShowAbout() {
         openDrawer();
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_about));
-        onView(withId(R.id.about_textview)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.about_textview)).check(matches(isDisplayed()));
         closeDrawer();
     }
 
