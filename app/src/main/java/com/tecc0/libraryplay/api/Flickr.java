4fd9422a7e781b79406ce7f679bfddf68b0f7786
Tecco <glaypppgod56d@gmail.com>
2016-03-28 16:41:21 +0900

    delete unnecessary import

--- app/src/main/java/com/tecc0/libraryplay/api/Flickr.java
+++ app/src/main/java/com/tecc0/libraryplay/api/Flickr.java
@@ -2,8 +2,6 @@ package com.tecc0.libraryplay.api;
 
 import com.google.gson.annotations.SerializedName;
 
-import java.util.List;
-
 /**
  * Created by makoto.nishimoto on 2016/03/03.
  */
--- app/src/main/java/com/tecc0/libraryplay/api/Weather.java
+++ app/src/main/java/com/tecc0/libraryplay/api/Weather.java
@@ -3,7 +3,6 @@ package com.tecc0.libraryplay.api;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by makoto.nishimoto on 2016/03/03.
